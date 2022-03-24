a956a78c8559a5744e77d9cb9ed1b833fe2eed12
Eric Do <ericdo.617@gmail.com>
2022-03-24 14:57:32 -0700

    Add links to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Center, HStack, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+const pages = [
+  {
+    title: 'About',
+    url: '/about'
+  },
+  {
+    title: 'Careers',
+    url: '/careers'
+  },
+  {
+    title: 'Products',
+    url: '/products'
+  },
+]
 
 const Navbar = () => {
   return (
-    <Box>
-      <Text>Navbar</Text>
-    </Box>
+    <Center>
+      <HStack>
+        {
+          pages.map(p => (
+            <NextLink
+              href={p.url}
+              key={p.url}
+              passHref
+            >
+              <Link>
+                { p.title }
+              </Link>
+            </NextLink>
+          ))
+        }
+      </HStack>
+    </Center>
   )
 };
 

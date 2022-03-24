5bb1ac987b7484ab63ea8d5216b90660d53a407f
Eric Do <ericdo.617@gmail.com>
2022-03-24 14:25:38 -0700

    Add page files

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const About = () => {
+  return (
+    <Box>
+      <Text>About</Text>
+    </Box>
+  )
+}
+
+export default About;
---
+++ pages/products.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const Products = () => {
+  return (
+    <Box>
+      <Text>About</Text>
+    </Box>
+  )
+}
+
+export default Products;

24aafe95bbca2f68454c99a19437d0bf3699b947
Eric Do <ericdo.617@gmail.com>
2022-03-25 13:55:56 -0700

    Fix alignment of collapsed items

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -6,16 +6,15 @@ import {
   Spacer,
   Heading,
   Text,
-  Center,
   HStack,
   VStack,
   useDisclosure,
   IconButton,
-  Collapse
+  Collapse,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
-import { Link } from '@/components/ui'
+import { Link } from '@/components/ui';
 
 export const NAV_ITEMS = [
   {
@@ -38,51 +37,67 @@ export const contact = 'Contact Us';
 const Navbar = () => {
   const { isOpen, onToggle } = useDisclosure();
   return (
-    <Flex alignItems={'center'} p={4}>
-      <Box>
-        <Link
-          src='/'
+    <VStack>
+      <Flex
+        alignItems={'flex-start'}
+        p={4}
+        w='100%'
+      >
+        <Box>
+          <Link src='/'>
+            <Heading>{brand}</Heading>
+          </Link>
+        </Box>
+        <Spacer />
+        <Flex
+          justifyContent='space-between'
+          display={{
+            base: 'none',
+            md: 'flex',
+          }}
         >
-          <Heading>{brand}</Heading>
-        </Link>
+          <DesktopNav />
+          <Button colorScheme={'blue'}>{contact}</Button>
+        </Flex>
+        <VStack>
+          <Flex
+            flex={{ base: 1, md: 'auto' }}
+            justifyContent='flex-end'
+            display={{
+              base: 'flex',
+              md: 'none',
+            }}
+          >
+            <IconButton
+              onClick={onToggle}
+              variant='ghost'
+              aria-label='Toggle navigation'
+              icon={
+                isOpen ? (
+                  <CloseIcon
+                    w={3}
+                    h={3}
+                  />
+                ) : (
+                  <HamburgerIcon
+                    w={5}
+                    h={5}
+                  />
+                )
+              }
+            />
+          </Flex>
+        </VStack>
+      </Flex>
+      <Box w='100%'>
+        <Collapse
+          in={isOpen}
+          animateOpacity
+        >
+          <MobileNav onToggle={onToggle} />
+        </Collapse>
       </Box>
-      <Spacer />
-      <Flex
-        justifyContent='space-between'
-        display={{
-          base: 'none',
-          md: 'flex',
-        }}
-      >
-        <DesktopNav />
-        <Button colorScheme={'blue'}>{contact}</Button>
-      </Flex>
-      <VStack>
-      <Flex
-        flex={{ base: 1, md: 'auto' }}
-        justifyContent='flex-end'
-        display={{
-          base: 'flex',
-          md: 'none'
-        }}
-      >
-        <IconButton
-          onClick={onToggle}
-          variant='ghost'
-          aria-label='Toggle navigation'
-          icon={
-            isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-          }
-        />
-      </Flex>
-      <Collapse
-        in={isOpen}
-        animateOpacity
-      >
-        <MobileNav />
-      </Collapse>
-      </VStack>
-    </Flex>
+    </VStack>
   );
 };
 
@@ -94,7 +109,10 @@ const DesktopNav = () => {
       px={14}
     >
       {NAV_ITEMS.map((p) => (
-        <Link src={p.url} key={p.url}>
+        <Link
+          src={p.url}
+          key={p.url}
+        >
           <Text fontSize={'lg'}>{p.title}</Text>
         </Link>
       ))}
@@ -102,24 +120,40 @@ const DesktopNav = () => {
   );
 };
 
-const MobileNav = () => {
+const MobileNav = ({ onToggle }: { onToggle: () => void }) => {
   return (
     <VStack
       p={4}
+      width={'100%'}
       display={{ md: 'none' }}
+      borderWidth={1}
     >
-      {NAV_ITEMS.map(navItem => (
+      {NAV_ITEMS.map((navItem) => (
         <Link
-         src={navItem.url}
-         key={navItem.url}
+          src={navItem.url}
+          key={navItem.url}
         >
-          <Box>
+          <Box
+            textAlign={'center'}
+            onClick={onToggle}
+            p={1}
+          >
             {navItem.title}
           </Box>
         </Link>
       ))}
+      <Flex
+        justifyContent={'center'}
+      >
+        <Button
+          colorScheme={'blue'}
+          alignSelf='center'
+        >
+          {contact}
+        </Button>
+      </Flex>
     </VStack>
-  )
-}
+  );
+};
 
 export default Navbar;
---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const Contact = () => {
+  return (
+    <Box>
+      <Text>Contact Us</Text>
+    </Box>
+  )
+}
+
+export default Contact;

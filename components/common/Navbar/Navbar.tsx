d43b239a28f1f4b06ff482803fd8fa0aebcb90d4
Eric Do <ericdo.617@gmail.com>
2022-03-25 00:10:04 -0700

    Update navbar responsiveness

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -9,6 +9,7 @@ import {
   Center,
   HStack,
   useDisclosure,
+  IconButton,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link';
@@ -33,7 +34,7 @@ export const brand = 'Brand';
 export const contact = 'Contact Us';
 
 const Navbar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onToggle } = useDisclosure();
   return (
     <Flex alignItems={'center'} p={4}>
       <Box>
@@ -45,24 +46,42 @@ const Navbar = () => {
       </Box>
       <Spacer />
       <Flex
+        flex={{ base: 1, md: 'auto' }}
+        justifyContent='flex-end'
+        display={{
+          base: 'flex',
+          md: 'none'
+        }}
+      >
+        <IconButton
+          onClick={onToggle}
+          aria-label='Toggle navigation'
+          icon={
+            isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+          }
+        />
+      </Flex>
+      <Flex
+        justifyContent='space-between'
         display={{
           base: 'none',
           md: 'flex',
         }}
       >
         <DesktopNav />
-      </Flex>
-      <Spacer />
-      <Box>
         <Button colorScheme={'blue'}>{contact}</Button>
-      </Box>
+      </Flex>
     </Flex>
   );
 };
 
 const DesktopNav = () => {
   return (
-    <HStack spacing={14} justifyContent={'space-between'}>
+    <HStack
+      spacing={14}
+      justifyContent={'space-between'}
+      px={14}
+    >
       {pages.map((p) => (
         <Link src={p.url} key={p.url}>
           <Text fontSize={'lg'}>{p.title}</Text>
